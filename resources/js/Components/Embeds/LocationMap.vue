91aa68bb224a531261f33f44a5e9006172f4db2d
Ahsan AUA <61789531+AUAboi@users.noreply.github.com>
2022-11-20 20:11:00 +0500

    frontpage additions

---
+++ resources/js/Components/Embeds/LocationMap.vue
@@ -0,0 +1,6 @@
+<template>
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31872.584663019108!2d101.5168430899047!3d3.075156270821062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4da088c300c5%3A0x57ff584a260e662d!2sMenara%20U2%2C%2040100%20Shah%20Alam%2C%20Selangor%2C%20Malaysia!5e0!3m2!1sen!2s!4v1668953951330!5m2!1sen!2s"
+    width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade"></iframe>
+</template>
--- resources/js/Pages/Public/Home.vue
+++ resources/js/Pages/Public/Home.vue
@@ -1,31 +1,47 @@
 
 <script setup>
-import { Link } from '@inertiajs/inertia-vue3';
+import { Head, Link } from '@inertiajs/inertia-vue3';
 
-
+import LocationMap from "@/Components/Embeds/LocationMap.vue"
 
 </script>
 
 <template>
-  <div class="hero-bg  ">
-    <div class="bg-black bg-opacity-50 sm:pl-3 py-20">
+
+  <Head title="Home"></Head>
+  <section class="hero-bg ">
+    <div class="bg-black bg-opacity-70 sm:pl-3 py-40">
       <div>
-        <h1 class="text-white text-2xl sm:text-5xl md:text-6xl font-bold">Welcome to Cafe</h1>
-        <h2 class="text-white text-2xl sm:text-5xl md:text-6xl font-bold">Enjoy your time</h2>
+        <h1 class="text-white text-5xl md:text-6xl font-bold">Welcome to Cafe</h1>
+        <h2 class="text-white text-5xl md:text-6xl font-bold">Enjoy your time</h2>
       </div>
       <div class="mt-6  cursor-pointer">
-        <Link href="#" as="button"
-          class="bg-yellow-400 transition-colors  duration-150 hover:bg-yellow-600 text-black text-sm sm:text-lg sm:py-2 sm:px-4 font-bold mr-3 py-1 px-2 rounded-full">
+        <Link :href="route('menu')" as="button"
+          class="bg-yellow-400 transition-colors  duration-150 hover:bg-yellow-600 text-black text-lg sm:py-2 sm:px-4 font-bold mr-3 py-1 px-2 rounded-full">
+        Order now
+        </Link>
+        <Link :href="route('category')" as="button"
+          class="border cursor-pointer transition-colors  duration-150 border-yellow-600 hover:bg-yellow-600 text-white text-lg sm:py-2 sm:px-4 font-bold py-1 px-2 rounded-full">
+
         Browse Categories
         </Link>
-        <Link href="#" as="button"
-          class="border cursor-pointer transition-colors  duration-150 border-yellow-600 hover:bg-yellow-600 text-white text-sm sm:text-lg sm:py-2 sm:px-4 font-bold py-1 px-2 rounded-full">
-        Contact Us
-        </Link>
+      </div>
+      <div class="text-white mt-8 ml-2 text-2xl">
+        We are proud to serve you the best
       </div>
     </div>
 
-  </div>
+  </section>
+  <section>
+    <div class="font-extrabold sm:text-3xl shadow-xl text-2xl md:text-5xl m-6 fancy-font ">
+      <h2 class="text-center tracking-wider bg-yellow-300 p-2">Order Now or Visit us </h2>
+    </div>
+    <div class="text-center text-xl p-2 m-2">
+      <h3 class="text-2xl py-2">We are located at</h3>
+      <LocationMap class="mx-auto" />
+    </div>
+
+  </section>
 </template>
 
 <style scoped>
